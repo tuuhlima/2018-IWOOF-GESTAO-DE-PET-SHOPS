ed091c5780c7d6710d6dda0d6f3582267f8d6241
Brayan Santos <v.brayansantos@gmail.com>
2018-02-25 11:56:51 -0300

    Implamentação search

--- iwoof-mobile/src/pages/home/home.ts
+++ iwoof-mobile/src/pages/home/home.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { } from '@types/googlemaps';
+import { Component, NgZone } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation';
+import { LoadingController } from 'ionic-angular';
+import { } from '@types/googlemaps'; /* import google maps*/
 
 @Component({
   selector: 'page-home',
@@ -8,17 +10,44 @@ import { } from '@types/googlemaps';
 })
 export class HomePage {
 
+  autocompleteItems: any;
+  autocomplete: any;
+  GoogleAutocomplete: any;
   map: any; 
   google: any;
-  constructor(public navCtrl: NavController) {
 
+  constructor(
+    public navCtrl: NavController,
+    public zone: NgZone,
+    public geolocation: Geolocation,
+    public loadingCtrl: LoadingController
+  ) {
+    this.autocomplete = { input: '' };
+    this.autocompleteItems = [];
   }
-
+  
   ionViewDidEnter(){
     //Set latitude and longitude of some places
     this.map = new google.maps.Map(document.getElementById('map'), {
       center: { lat: -34.9011, lng: -56.1645 },
       zoom: 15
+    });
+    this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
+  }
+
+  updateSearchResults(){
+    if (this.autocomplete.input == '') {
+      this.autocompleteItems = [];
+      return;
+    }
+    this.GoogleAutocomplete.getPlacePredictions({ input: this.autocomplete.input },
+    (predictions, status) => {
+      this.autocompleteItems = [];
+      this.zone.run(() => {
+        predictions.forEach((prediction) => {
+          this.autocompleteItems.push(prediction);
+        });
+      });
     });
   }
 
